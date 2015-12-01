ec5a2756d3160e1aff272e3c4b3a761567e03ad5
Chupligin Sergey <neochapay@gmail.com>
2015-12-01 14:08:26 +0300

    Update mini calendar module

--- modules/mod_calendar_mini/module.php
+++ modules/mod_calendar_mini/module.php
@@ -21,7 +21,7 @@ function  mod_calendar_mini($module_id)
 
   foreach($events_raw as $item)
   {
-    $events[date("d-m-Y",$item['start_time'])][] = $item;
+    $events[date("j-m-Y",$item['start_time'])][] = $item;
   }
 
   $dayofmonth = date('t');
@@ -65,8 +65,50 @@ function  mod_calendar_mini($module_id)
       break;
     }
   }
+  
+  setlocale(LC_ALL, 'ru_RU');
+  
+  switch(date("m",time()))
+  {
+    case 1:
+        $month_n = "Январь";
+        break;
+    case 2:
+        $month_n = "Февраль";
+        break;
+    case 3:
+        $month_n = "Март";
+        break;
+    case 4:
+        $month_n = "Апрель";
+        break;
+    case 5:
+        $month_n = "Май";
+        break;
+    case 6:
+        $month_n = "Июнь";
+        break;
+    case 7:
+        $month_n = "Июль";
+        break;
+    case 8:
+        $month_n = "Август";
+        break;
+    case 9:
+        $month_n = "Сентябрь";
+        break;
+    case 10:
+        $month_n = "Октябрь";
+        break;
+    case 11:
+        $month_n = "Ноябрь";
+        break;
+    case 12:
+        $month_n = "Декабрь";
+        break;
+  }
 
-//   echo '<h3 class="module_calendar_mini">Месяц</h3>';
+  echo '<h3 class="module_calendar_mini">'.$month_n.'</h3>';
   echo '<table class="module_calendar_mini_table">';
   for($i = 0; $i < count($week); $i++)
   {
@@ -81,7 +123,7 @@ function  mod_calendar_mini($module_id)
 	  $event_text = '<div class="eventlist"><ul>';
 	  foreach($events[$week[$i][$j]."-".$month."-".$year] as $event)
 	  {
-	    $event_text .= '<li><a href="/calendar/event'.$event['id'].'.html">'.$event['title'].'</a></li>';
+	    $event_text .= '<li data-event-id="'.$event['id'].'" style="background-color: '.$event['bg'].'; color:'.$event['tx'].';"><a style="background-color: '.$event['bg'].'; color:'.$event['tx'].';" href="/calendar/event'.$event['id'].'.html">'.$event['title'].'</a></li>';
 	  }
 	  $event_text .= '</ul></div>';
 	}
